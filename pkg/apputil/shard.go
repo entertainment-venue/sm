736d2fb416462c063df8ffd2ffde094e83d31c28
zd3tl <gositaka@gmail.com>
2021-09-14 16:05:38 +0800

    add licence header

--- pkg/apputil/shard.go
+++ pkg/apputil/shard.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The entertainment-venue Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package apputil
 
 import (
@@ -48,7 +62,9 @@ type ShardOpMessage struct {
 type OpType int
 
 const (
-	OpTypeAdd OpType = iota
+	// 0保留，因为go默认int的值是0，防止无意识的错误
+	// 参考：https://github.com/etcd-io/etcd/blob/main/client/v3/op.go#L22
+	OpTypeAdd OpType = iota + 1
 	OpTypeDrop
 )
 
