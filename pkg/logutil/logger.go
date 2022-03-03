7ed290b0a20d74beb04b0836f446c521637aee94
juexin <zhengjuexin@sfmail.sf-express.com>
2022-03-03 17:44:53 +0800

    add logger for util

---
+++ pkg/logutil/logger.go
@@ -0,0 +1,12 @@
+package logutil
+
+type Logger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+}
