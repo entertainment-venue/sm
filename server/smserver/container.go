51d3238e91c2128173021140321633c872dc5d36
wkwen <38279458+wkwen@users.noreply.github.com>
2022-04-22 15:18:18 +0800

    add health api

--- server/smserver/container.go
+++ server/smserver/container.go
@@ -141,6 +141,7 @@ func (c *smContainer) getHttpHandlers() map[string]func(c *gin.Context) {
 	handlers["/sm/server/add-shard"] = apiSrv.GinAddShard
 	handlers["/sm/server/del-shard"] = apiSrv.GinDelShard
 	handlers["/sm/server/get-shard"] = apiSrv.GinGetShard
+	handlers["/sm/server/health"] = apiSrv.GinHealth
 	handlers["/swagger/*any"] = ginSwagger.WrapHandler(swaggerfiles.Handler)
 	return handlers
 }
