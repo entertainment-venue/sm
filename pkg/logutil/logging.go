0e22055d1bc4a0bb26c352b2d260197520424e5e
wkwen <38279458+wkwen@users.noreply.github.com>
2022-04-22 19:06:39 +0800

    log

---
+++ pkg/logutil/logging.go
@@ -0,0 +1,100 @@
+package logutil
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type logRotationConfig struct {
+	*lumberjack.Logger
+}
+
+type LogOptions struct {
+	Path       string
+	MaxSize    int
+	MaxBackups int
+	MaxAge     int
+	Output     string
+}
+
+var defaultLogOptions = LogOptions{
+	Path:       "./logs/sm.log",
+	MaxSize:    1024,
+	MaxBackups: 50,
+	MaxAge:     3,
+	Output:     "stdout",
+}
+
+type logOptionsFunc func(*LogOptions)
+
+func WithPath(v string) logOptionsFunc {
+	return func(o *LogOptions) {
+		o.Path = v
+	}
+}
+
+func WithMaxSize(v int) logOptionsFunc {
+	return func(o *LogOptions) {
+		o.MaxSize = v
+	}
+}
+
+func WithMaxBackups(v int) logOptionsFunc {
+	return func(o *LogOptions) {
+		o.MaxBackups = v
+	}
+}
+
+func WithMaxAge(v int) logOptionsFunc {
+	return func(o *LogOptions) {
+		o.MaxAge = v
+	}
+}
+
+func WithOutput(v string) logOptionsFunc {
+	return func(o *LogOptions) {
+		o.Output = v
+	}
+}
+
+// Sync implements zap.Sink
+func (logRotationConfig) Sync() error { return nil }
+
+func NewLogger(opt ...logOptionsFunc) (*zap.Logger, error) {
+	opts := defaultLogOptions
+	for _, o := range opt {
+		o(&opts)
+	}
+
+	cfg := logRotationConfig{
+		&lumberjack.Logger{
+			// 每个文件1g
+			MaxSize: opts.MaxSize,
+
+			// 50g文件
+			MaxBackups: opts.MaxBackups,
+
+			// 最多保留3天
+			MaxAge: opts.MaxAge,
+		},
+	}
+	if err := zap.RegisterSink("rotate", func(u *url.URL) (zap.Sink, error) {
+		cfg.Filename = u.Path[1:]
+		return &cfg, nil
+	}); err != nil {
+		return nil, errors.Wrap(err, "")
+	}
+
+	zap.AddCallerSkip(1)
+	zapCfg := zap.NewProductionConfig()
+	zapCfg.OutputPaths = []string{fmt.Sprintf("rotate://%s",opts.Path), opts.Output}
+	logger, err := zapCfg.Build()
+	if err != nil {
+		return nil, errors.Wrap(err, "")
+	}
+	return logger, nil
+}
