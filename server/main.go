ff60c7427bd142e4cf331b609ff4047b04a2f499
zhongweikang <zwk5371@163.com>
2022-10-18 10:07:31 +0800

    sm server entry needn't opts (#66)

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 
 // main 默认程序入口，也可以直接使用 smmain.StartSM 传入自定义的 smmain.Resolver
 func main() {
-	if err := smmain.StartSM(nil); err != nil {
+	if err := smmain.StartSM(); err != nil {
 		panic(err)
 	}
 }
